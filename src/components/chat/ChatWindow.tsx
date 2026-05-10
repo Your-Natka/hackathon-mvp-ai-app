215d4473762fffbf0b62a4033f14a04d3a31a0a6
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2026-05-10 13:04:53 +0200

    MessageBubble

--- src/components/chat/ChatWindow.tsx
+++ src/components/chat/ChatWindow.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { useChat } from "@/hooks/useChat";
 import { ChatInput } from "./ChatInput";
 import { MessageBubble } from "./MessageBubble";
 
 export function ChatWindow() {
-  const { messages, sendMessage, loading } = useChat();
-
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex-1 overflow-y-auto p-4">
-        {messages.map((m, i) => (
-          <MessageBubble key={i} message={m} />
-        ))}
-      </div>
-
-      <ChatInput onSend={sendMessage} loading={loading} />
+    <div>
+      <MessageBubble />
+      <ChatInput />
     </div>
   );
 }
